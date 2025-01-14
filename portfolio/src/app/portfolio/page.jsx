dba86aee2b555c83a8e6fab49c7082e084b2af22
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2025-01-15 01:04:08 +0500

    minor changes

--- portfolio/src/app/portfolio/page.jsx
+++ portfolio/src/app/portfolio/page.jsx
@@ -8,7 +8,7 @@ const Portfolio = () => {
     <div className=" mt-12 bg-gray-900 hover:bg-gradient-to-r from-cyan-800 via-purple-800 to-pink-800 cursor-pointer rounded-lg shadow-lg p-6 transform transition duration-300 hover:scale-105 hover:shadow-2xl">
       <a href="// https://drive.google.com/drive/folders/1fA8yjK86w4P6J3_HllCsI8bW0eeJP2cN?usp=sharing" target="_blank" rel="noopener noreferrer">Click here to see the portfolio </a>
     </div>
-    <h3 className='mt-28 ml-8 lg:ml-16 sm:text-sm md:text-xl lg:text-2xl font-bold'>Social Links</h3>
+    <h3 className=''>Social Links</h3>
 
 
     </div>
